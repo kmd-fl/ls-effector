ab9aa37a77a0b60171c7d912180107f36f0f515c
Maria Kuklina <maria@fluence.one>
2024-03-20 16:40:04 +0100

    fix: fixed flow

--- build.sh
+++ build.sh
@@ -7,4 +7,7 @@ cd "$(dirname "$0")"
 # This script builds all subprojects and puts all created Wasm modules in one dir
 fluence module build ./effector --no-input
 
+mkdir -p cid/artifacts
+ipfs add --offline -Q --only-hash --cid-version 1 --hash sha2-256 --chunker=size-262144 target/wasm32-wasi/release/ls_effector.wasm > cid/artifacts/cidv1
+
 cargo build --release
---
+++ validate.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+set -o errexit -o nounset -o pipefail
+
+# set current working directory to script directory to run script from everywhere
+cd "$(dirname "$0")"
+
+
+# Build the project
+# This script builds all subprojects and puts all created Wasm modules in one dir
+fluence module build ./effector --no-input
+
+mkdir -p cid/artifacts
+ipfs add --offline -Q --only-hash --cid-version 1 --hash sha2-256 --chunker=size-262144 target/wasm32-wasi/release/ls_effector.wasm > cid/artifacts/cidv1
+
+cd cid
+cargo build --release
+cd ..
+
+# Pack the module
+fluence module pack ./effector/ --binding-crate=./imports/ --no-input -d .
+
+echo "Extracting the CID from the package and the crate"
+cid_package="$(tar -axf ls_effector.tar.gz module.yaml -O | grep cid | cut -d' ' -f2)"
+cid_crate="$(cut -d' ' -f2 < cid/artifacts/cidv1)"
+
+if [[ "$cid_package" = "$cid_crate" ]]
+then
+   echo "Validated"
+else
+   echo "Not validated" >&2
+   echo "Fluence Package CID: '${cid_package}'" >&2
+   echo "Rust Crate CID: '${cid_crate}'" >&2
+   exit 1
+fi
